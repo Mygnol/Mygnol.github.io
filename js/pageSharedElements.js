fb9fb516044198f7c15b94dac27204a601aded4e
Mygnol <lymprivateacc@gmail.com>
2019-11-23 00:59:27 +0800

    =Fixed index stuff +Added Contacts

--- js/pageSharedElements.js
+++ js/pageSharedElements.js
@@ -60,7 +60,7 @@ function makeHeader(){
 			document.write('<a href="index.html"><div class = "buttonHome"></div></a>');
 			document.write('<a href="about.html"><div class = "buttonAbout"></div></a>');
 			document.write('<a href="projects.html"><div class = "buttonProjects"></div></a>');
-			document.write('<a href="contact.html"><div class = "buttonContact"></div></a>');
+			document.write('<a href="about.html#contact"><div class = "buttonContact"></div></a>');
 		document.write('</ul>');
 
 		document.write('<img src = "images/lineBreak.png" class = "lineBreak">');
