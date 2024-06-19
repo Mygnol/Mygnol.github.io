7f6289a999bfecd3d4f291bb3c4acfd4a0c82bfd
Long YM <40413261+Mygnol@users.noreply.github.com>
2024-06-19 15:48:53 +0800

    Update pageSharedElements.js

--- js/pageSharedElements.js
+++ js/pageSharedElements.js
@@ -64,13 +64,15 @@ function makeHeader(){
 	//NAVBAR
 	document.write('<nav id="navbar">');
 		//document.write('<img src = "images/lineBreak.png" class = "lineBreak">');
-
+			
+		document.write('<div class = "spacer"></div>');
 		document.write('<ul>');
 			document.write('<a href="index.html"><div class = "buttonHome"></div></a>');
 			document.write('<a href="about.html"><div class = "buttonAbout"></div></a>');
 			document.write('<a href="projects.html"><div class = "buttonProjects"></div></a>');
 			document.write('<a href="about.html#contact"><div class = "buttonContact"></div></a>');
 		document.write('</ul>');
+		document.write('<div class = "spacer"></div>');
 
 		//document.write('<img src = "images/lineBreak.png" class = "lineBreak">');
 	document.write('</nav>');
